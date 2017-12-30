daea1de4b1f69c4bfb5af31f169bd2b8f4605b59
grobovsky <30655667+grobovsky@users.noreply.github.com>
2017-12-30 08:07:27 +0300

    Update Solution.java

--- 2.JavaCore/src/com/javarush/task/task18/task1816/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1816/Solution.java
@@ -9,24 +9,16 @@ import java.util.ArrayList;
 
 public class Solution {
     public static void main(String[] args) throws IOException {
-        ArrayList<Integer> fileContent = new ArrayList<>();
         ArrayList<Integer> alphabet = new ArrayList<>();
         for(int i = 0; i < 58; i++){
             if(i < 26 || i >= 32) {
                 alphabet.add(i + 65);
             }
         }
-        //System.out.print(alphabet.toString());
-        if(args[0] != null){
-            FileInputStream stream = new FileInputStream(args[0]);
-            while (stream.available() > 0){
-                fileContent.add(stream.read());
-            }
-            stream.close();
-        }
+        FileInputStream stream = new FileInputStream(args[0]);
         int count = 0;
-        for(int i = 0; i < fileContent.size(); i++){
-            if(alphabet.contains(fileContent.get(i))){
+        while(stream.available() > 0){
+            if(alphabet.contains(stream.read())){
                 count++;
             }
         }
