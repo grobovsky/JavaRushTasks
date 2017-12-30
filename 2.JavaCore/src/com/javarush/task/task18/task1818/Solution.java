60264de28b77388a7f95cffa8b276ae347c096dd
grobovsky <30655667+grobovsky@users.noreply.github.com>
2017-12-30 12:06:22 +0300

    Update Solution.java

--- 2.JavaCore/src/com/javarush/task/task18/task1818/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1818/Solution.java
@@ -21,15 +21,13 @@ public class Solution {
         byte[] buff3 = new byte[in3.available()];
         in2.read(buff2);
         in3.read(buff3);
-        BufferedOutputStream out2 = new BufferedOutputStream(new FileOutputStream(f1));
-        BufferedOutputStream out3 = new BufferedOutputStream(new FileOutputStream(f1, true));
-        out2.write(buff2, 0, buff2.length);
-        out3.write(buff3, 0, buff3.length);
+        
+        BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(f1, true));
+        out.write(buff2, 0, buff2.length);
+        out.write(buff3, 0, buff3.length);
         scan.close();
         in2.close();
         in3.close();
-        out2.close();
-        out3.close();
-
+        out.close();
     }
 }
