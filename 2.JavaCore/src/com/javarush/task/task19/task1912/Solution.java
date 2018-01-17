6bde00e730861f823fd3c14afa2f60a045efbc94
grobovsky <30655667+grobovsky@users.noreply.github.com>
2018-01-17 15:53:06 +0300

    Create Solution.java

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1912/Solution.java
@@ -0,0 +1,37 @@
+package com.javarush.task.task19.task1912;
+
+/* 
+Ридер обертка 2
+
+В методе main подмените объект System.out написанной тобой ридер-оберткой по аналогии с лекцией.
+Твоя ридер-обертка должна заменять все подстроки "te" на "??".
+Вызови готовый метод printSomething(), воспользуйтесь testString.
+Верни переменной System.out первоначальный поток.
+Выведи модифицированную строку в консоль.
+*/
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+public class Solution {
+    public static TestString testString = new TestString();
+
+    public static void main(String[] args) {
+        PrintStream ps = System.out;
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        PrintStream stream = new PrintStream(out);
+        System.setOut(stream);
+        testString.printSomething();
+
+        System.setOut(ps);
+        String strOut = out.toString();
+        String res = strOut.replaceAll("te", "??");
+        System.out.println(res);
+    }
+
+    public static class TestString {
+        public void printSomething() {
+            System.out.println("it's a text for testing");
+        }
+    }
+}
