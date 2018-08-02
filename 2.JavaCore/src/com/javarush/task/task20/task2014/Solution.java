2c1b91a4405d20d40c454ce4db320d8dc413b4ba
grobovsky <30655667+grobovsky@users.noreply.github.com>
2018-08-02 21:51:49 +0300

    Create Solution.java

---
+++ 2.JavaCore/src/com/javarush/task/task20/task2014/Solution.java
@@ -0,0 +1,66 @@
+package com.javarush.task.task20.task2014;
+
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/* 
+Serializable Solution
+Сериализуй класс Solution.
+Подумай, какие поля не нужно сериализовать, пометь ненужные поля модификатором transient.
+Объект всегда должен содержать актуальные итоговые данные.
+Метод main не участвует в тестировании.
+
+Написать код проверки самостоятельно в методе main:
+1) создать файл, открыть поток на чтение (input stream) и на запись(output stream);
+2) создать экземпляр класса Solution - savedObject;
+3) записать в поток на запись savedObject (убедитесь, что они там действительно есть);
+4) создать другой экземпляр класса Solution с другим параметром;
+5) загрузить из потока на чтение объект - loadedObject;
+6) проверить, что savedObject.string равна loadedObject.string;
+7) обработать исключения.
+
+Требования:
+1. Поле pattern должно быть отмечено модификатором transient.
+2. Поле currentDate должно быть отмечено модификатором transient.
+3. Поле temperature должно быть отмечено модификатором transient.
+4. Поле string НЕ должно быть отмечено модификатором transient.
+*/
+
+public class Solution implements Serializable {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        System.out.println(new Solution(4));
+
+        Solution saveObject = new Solution(5);
+        ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("D:\\Java_Rush\\project\\file11.txt"));
+        out.writeObject(saveObject);
+        out.flush();
+        out.close();
+
+        ObjectInputStream in = new ObjectInputStream(new FileInputStream("D:\\Java_Rush\\project\\file11.txt"));
+        Solution loadedObject = (Solution) in.readObject();
+        in.close();
+
+
+        System.out.println(saveObject.string.equals(loadedObject.string));
+    }
+
+    private transient final String pattern = "dd MMMM yyyy, EEEE";
+    private transient Date currentDate;
+    private transient int temperature;
+    String string;
+
+    public Solution(int temperature) {
+        this.currentDate = new Date();
+        this.temperature = temperature;
+
+        string = "Today is %s, and current temperature is %s C";
+        SimpleDateFormat format = new SimpleDateFormat(pattern);
+        this.string = String.format(string, format.format(currentDate), temperature);
+    }
+
+    @Override
+    public String toString() {
+        return this.string;
+    }
+}
