89a71c0192c95d30cd25ff8197fd852d157a1243
grobovsky <30655667+grobovsky@users.noreply.github.com>
2019-08-10 18:54:01 +0300

    Create Shop.java

---
+++ 4.JavaCollections/src/com/javarush/task/task33/task3308/Shop.java
@@ -0,0 +1,21 @@
+package com.javarush.task.task33.task3308;
+
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+import java.util.ArrayList;
+import java.util.List;
+
+@XmlRootElement
+@XmlType(name = "shop")
+public class Shop {
+    public Goods goods;
+    public int count;
+    public double profit;
+    public String[] secretData;
+
+    @XmlRootElement
+    @XmlType(name = "goods")
+    static class Goods{
+        public List<String> names = new ArrayList<>();
+    }
+}
